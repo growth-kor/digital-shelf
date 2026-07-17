d2e352ce321b579643f628392e6d2aac15aacb48
growth-kor <sakteboard4335@gmail.com>
2026-07-17 20:39:10 +0900

    Trigger deployment with new base config comment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/digital-shelf/',
+  base: '/digital-shelf/', // GitHub Pages 배포용 base 경로 설정
 })
