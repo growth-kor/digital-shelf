ea60f7f9fadb9300e0ee22c9ce1637ffed152734
growth-kor <sakteboard4335@gmail.com>
2026-07-18 01:06:13 +0900

    Update Firebase Config to the new free project

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  // 캡처 화면에서 확인된 유저님의 진짜 API 키입니다.
-  apiKey: "AIzaSyBIKNdOGuzCLr30Nh3VS5bKkIBKXhKn4BE",
-  authDomain: "my-digital-library-aea5f.firebaseapp.com",
-  projectId: "my-digital-library-aea5f",
-  storageBucket: "my-digital-library-aea5f.firebasestorage.app",
-  messagingSenderId: "999674499400",
-  appId: "1:999674499400:web:90606eec2c735a921be37d",
-  measurementId: "G-SF8NS1T6ET"
+  apiKey: "AIzaSyC4LxOpnvYvCckH_OSm0mferD91ubWZ1xI",
+  authDomain: "digital-shelf-34e22.firebaseapp.com",
+  projectId: "digital-shelf-34e22",
+  storageBucket: "digital-shelf-34e22.firebasestorage.app",
+  messagingSenderId: "241469453078",
+  appId: "1:241469453078:web:c4d15cbb5f7ee5f10bd637"
 };
 
 const app = initializeApp(firebaseConfig);
